87236cf306e3a8757670c4a8c02e318708a47fff
Gabriel <gnarvaez85@gmail.com>
2023-12-08 18:28:03 -0300

    Desafio 2

--- productManager.js
+++ productManager.js
@@ -1,84 +1,123 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
+    constructor(filePath) {
+        this.path = filePath;
         this.products = [];
+        this.productIdCounter = 1;
+        this.loadProducts();
+    }
+
+    loadProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf8');
+            this.products = JSON.parse(data);
+
+            const maxId = this.products.reduce((max, product) => (product.id > max ? product.id : max), 0);
+            this.productIdCounter = maxId + 1;
+        } catch (error) {
+
+        }
+    }
+
+    saveProducts() {
+        const data = JSON.stringify(this.products, null, 2);
+        fs.writeFileSync(this.path, data);
+    }
+
+    addProduct(productData) {
+        const { title, description, price, thumbnail, code, stock } = productData;
+
+
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("Todos los campos son obligatorios.");
+            return;
+        }
+
+        if (this.products.some(product => product.code === code)) {
+            console.error("El código del producto ya existe.");
+            return;
+        }
+
+        const newProduct = {
+            id: this.productIdCounter++,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        };
+
+        this.products.push(newProduct);
+        this.saveProducts();
+        console.log("Producto agregado:", newProduct);
     }
 
     getProducts() {
         return this.products;
     }
 
-    addProduct(product) {
-        if (
-            !product.title ||
-            !product.description ||
-            !product.price ||
-            !product.thumbnail ||
-            !product.code ||
-            !product.stock
-        ) {
-            return 'Todos los campos son obligatorios.';
-        }
+    getProductById(id) {
+        const product = this.products.find(product => product.id === id);
 
-        const exists = this.products.find((prod) => prod.code === product.code);
-
-        if (exists) {
-            return 'El código del producto ya existe.';
-        }
-
-        if (this.products.length === 0) {
-            product.id = 1;
+        if (product) {
+            return product;
         } else {
-            product.id = this.products.length + 1;
+            console.error("Producto no encontrado. ID:", id);
         }
-
-        this.products.push(product);
-
-        return 'Producto agregado:', product;
     }
 
-    getProductById(pid) {
-        const result = this.products.find((prod) => prod.id === pid);
-        if (!result) {
-            return 'Producto no encontrado.';
+    updateProduct(id, updatedFields) {
+        const index = this.products.findIndex(product => product.id === id);
+
+        if (index !== -1) {
+            updatedFields.id = id;
+            this.products[index] = updatedFields;
+            this.saveProducts();
+            console.log("Producto actualizado:", updatedFields);
+        } else {
+            console.error("Producto no encontrado. ID:", id);
         }
-        return result;
+    }
+
+    deleteProduct(id) {
+        const index = this.products.findIndex(product => product.id === id);
+
+        if (index !== -1) {
+            const deletedProduct = this.products.splice(index, 1)[0];
+            this.saveProducts();
+            console.log("Producto eliminado:", deletedProduct);
+        } else {
+            console.error("Producto no encontrado. ID:", id);
+        }
     }
 }
 
-// Pruebas
+// Prueba
+const productManager = new ProductManager('productos.json');
+productManager.addProduct({
+    title: 'Producto Uno',
+    description: 'Descripción del producto Uno',
+    price: 19.99,
+    thumbnail: 'path/to/image.jpg',
+    code: 'P123',
+    stock: 50
+});
 
-const products = new ProductManager();
-console.log(
-    products.addProduct({
-        title: 'Producto 1',
-        description: 'Descripción del producto 1',
-        price: 29.99,
-        thumbnail: '/images/product1.jpg',
-        stock: 10,
-        code: 'abc123',
-    })
-);
+const allProducts = productManager.getProducts();
+console.log("Todos los productos:", allProducts);
 
-console.log(
-    products.addProduct({
-        title: 'Producto 2',
-        description: 'Descripción del producto 2',
-        price: 39.99,
-        thumbnail: '/images/product2.jpg',
-        stock: 15,
-        code: 'abc133',
-    })
-);
+const productId = 1;
+const productById = productManager.getProductById(productId);
+console.log("Producto por ID:", productById);
 
-console.log(
-    products.addProduct({
-        title: 'Producto 2',
-        description: 'Descripción del producto 2',
-        price: 39.99,
-        thumbnail: '/images/product2.jpg',
-        stock: 15,
+const updatedFields = {
+    title: 'Producto Modificado',
+    price: 29.99,
+    stock: 40
+};
+productManager.updateProduct(productId, updatedFields);
 
-    })
-);
-
-console.log(products.getProductById(1));
+const productIdToDelete = 2;
+productManager.deleteProduct(productIdToDelete);
